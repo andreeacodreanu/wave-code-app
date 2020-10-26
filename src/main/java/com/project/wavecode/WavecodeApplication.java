8184b3efe731c22be685563b871ed96ce0f6242b
Andreea <andrex_97@yahoo.com>
2020-10-26 22:33:20 +0200

    initial commit

---
+++ src/main/java/com/project/wavecode/WavecodeApplication.java
@@ -0,0 +1,13 @@
+package com.project.wavecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavecodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/wavecode/WavecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wavecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
